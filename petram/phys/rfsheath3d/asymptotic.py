3d13e5dc10b87cd23a5d1acb261df64627221b0a
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-06-04 14:07:42 -0400

    RF sheath 3D seems okay

--- petram/phys/rfsheath3d/asymptotic.py
+++ petram/phys/rfsheath3d/asymptotic.py
@@ -15,86 +15,78 @@ if use_parallel:
 else:
     import mfem.ser as mfem
 
+data = (('label1', VtableElement(None,
+                                 guilabel='Asymptotic BC',
+                                 default="",
+                                 tip="Dn = 0 (curl_t E_t = 0)")),)
+
 
 class RFsheath3D_Asymptotic(Domain, Phys):
     has_essential = False
     nlterms = []
     can_timedpendent = True
     has_3rd_panel = True
+    vt = Vtable(data)
 
     def __init__(self, **kwargs):
         super(RFsheath3D_Asymptotic, self).__init__(**kwargs)
 
     def attribute_set(self, v):
-        Bdry.attribute_set(self, v)
+        Domain.attribute_set(self, v)
         Phys.attribute_set(self, v)
         v['sel_readonly'] = False
         v['sel_index'] = []
         return v
 
-    @property
-    def vt(self):
-        root_phys = self.get_root_phys()
-        if not isinstance(root_phys, PhysModule):
-            data = (('esse_value', VtableElement(None, type="array",
-                                                 guilabel="dummy",
-                                                 default="0.0",
-                                                 tip="dummy")),)
-            return Vtable(data)
-
-        dep_vars = self.get_root_phys().dep_vars
-        if not hasattr(self, '_dep_var_bk'):
-            self._dep_var_bk = ""
-
-        if self._dep_var_bk != dep_vars:
-            dep_var = dep_vars[0]
-            data = (('esse_value', VtableElement("esse_value", type="array",
-                                                 guilabel=dep_var,
-                                                 default="0.0",
-                                                 tip="Reference for disance measurement")),)
-            vt = Vtable(data)
-            self._vt1 = vt
-            self._dep_var_bk = dep_vars
-            self.update_attribute_set()
-        else:
-            vt = self._vt1
-        return vt
-
-    def get_essential_idx(self, kfes):
+    def has_bf_contribution(self, kfes):
         if kfes == 0:
-            return self._sel_index
+            return True
         else:
-            return []
+            return False
 
-    def apply_essential(self, engine, gf, real=False, kfes=0):
-        if kfes > 0:
+    def has_mixed_contribution(self):
+        return True
+
+    def get_mixedbf_loc(self):
+        '''
+        [ curl-curl + mass          curl^][  E  ] = [J]
+        [ div                  dif       ][ Vsh ] = [0]
+        [ curl                           ][ Fmg ] = [0]
+        '''
+        Vshname = self.get_root_phys().dep_vars[0]
+        Fmgname = self.get_root_phys().dep_vars[1]
+
+        paired_var = self.get_root_phys().paired_var
+        mfem_physroot = self.get_root_phys().parent
+        var_s = mfem_physroot[paired_var[0]].dep_vars
+        Ename = var_s[paired_var[1]]
+
+        loc = ((Fmgname, Ename, 1, 1),
+               (Fmgname, Ename, -1, 1),
+               (Vshname, Ename, 1, 1),)
+        return loc
+
+    def add_bf_contribution(self, engine, a, real=True, kfes=0):
+        from petram.phys.rfsheath3d.asymptotic_subs import add_bf_contribution
+
+        if kfes != 0:
             return
-        c0 = self.vt.make_value_or_expression(self)[0]
 
         if real:
-            dprint1("Apply Ess.(real)" + str(self._sel_index), 'c0', c0)
+            dprint1("Add diffusion integrator contribution(real)")
         else:
-            dprint1("Apply Ess.(imag)" + str(self._sel_index), 'c0', c0)
+            dprint1("Add diffusion integrator contribution(imag)")
 
-        name = self.get_root_phys().dep_vars[0]
-        fes = engine.get_fes(self.get_root_phys(), name=name)
+        add_bf_contribution(self, mfem, engine, a, real=real, kfes=kfes)
 
-        fec_name = fes.FEColl().Name()
+    def add_mix_contribution2(self, engine, mbf, r, c,  is_trans, _is_conj,
+                              real=True):
+        if real:
+            dprint1("Add mixed contribution(real)"  "r/c", r, c, is_trans)
+        else:
+            dprint1("Add mixed contribution(imag)"  "r/c", r, c, is_trans)
 
-        mesh = engine.get_emesh(mm=self)
-        ibdr = mesh.bdr_attributes.ToList()
+        from petram.phys.rfsheath3d.asymptotic_subs import add_mix_contribution2
 
-        bdr_attr = [0]*mesh.bdr_attributes.Max()
-        for idx in self._sel_index:
-            bdr_attr[idx-1] = 1
-
-        ess_vdofs = mfem.intArray()
-        fes.GetEssentialVDofs(mfem.intArray(bdr_attr), ess_vdofs, 0)
-        vdofs = np.where(np.array(ess_vdofs.ToList()) == -1)[0]
-        dofs = mfem.intArray([fes.VDofToDof(i) for i in vdofs])
-        fes.BuildDofToArrays()
-
-        coeff1 = SCoeff(c0, self.get_root_phys().ind_vars,
-                        self._local_ns, self._global_ns,
-                        real=real)
-        gf.ProjectCoefficient(coeff1, dofs, 0)
+        add_mix_contribution2(self, mfem, engine, mbf, r, c,  is_trans, _is_conj,
+                              real=real)
--- petram/phys/rfsheath3d/rfsheath3d_model.py
+++ petram/phys/rfsheath3d/rfsheath3d_model.py
@@ -14,7 +14,6 @@ from petram.phys.phys_model import Phys, PhysModule
 import petram.debug as debug
 dprint1, dprint2, dprint3 = debug.init_dprints('RFSheath3D_Model')
 
-txt_predefined = ''
 model_basename = 'RFsheath3D'
 
 try:
@@ -23,6 +22,7 @@ except ImportError:
     import petram.mfem_model as mm
     if mm.has_addon_access not in ["any", "rfsheath"]:
         sys.modules[__name__].dependency_invalid = True
+
 
 class RFsheath3D_DefDomain(Domain, Phys):
     can_delete = False
@@ -35,6 +35,7 @@ class RFsheath3D_DefDomain(Domain, Phys):
 
     def import_panel1_value(self, v):
         pass
+
 
 class RFsheath3D_DefBdry(Bdry, Phys):
     can_delete = False
@@ -53,6 +54,7 @@ class RFsheath3D_DefBdry(Bdry, Phys):
     def get_possible_bdry(self):
         return []
 
+
 class RFsheath3D_DefPoint(Point, Phys):
     can_delete = False
     is_essential = False
@@ -66,6 +68,7 @@ class RFsheath3D_DefPoint(Point, Phys):
         v['sel_readonly'] = False
         v['sel_index'] = ['']
         return v
+
 
 class RFsheath3D_DefPair(Pair, Phys):
     can_delete = False
@@ -82,8 +85,10 @@ class RFsheath3D_DefPair(Pair, Phys):
         v['sel_index'] = []
         return v
 
+
 class RFsheath3D(PhysModule):
     dim_fixed = True
+
     def __init__(self, **kwargs):
         super(RFsheath3D, self).__init__()
         Phys.__init__(self)
@@ -92,13 +97,13 @@ class RFsheath3D(PhysModule):
 
     @property
     def dep_vars(self):
-        ret = ["Vsh"+self.dep_vars_suffix,
-               "Fmag"+self.dep_vars_suffix]
+        ret = self.dep_vars_base
+        ret = [x + self.dep_vars_suffix for x in ret]
         return ret
 
     @property
     def dep_vars_base(self):
-        val = self.dep_vars_base_txt.split(',')[0]
+        val = self.dep_vars_base_txt.split(',')
         return val
 
     @property
@@ -118,6 +123,12 @@ class RFsheath3D(PhysModule):
     def vdim(self, val):
         pass
 
+    def fes_order(self, idx):
+        self.vt_order.preprocess_params(self)
+        if idx == 0:
+            return self.order
+        return self.order-1
+
     def get_fec_type(self, idx):
         '''
         H1 
@@ -125,13 +136,13 @@ class RFsheath3D(PhysModule):
         ND
         RT
         '''
-        values = ['L2', 'H1']
+        values = ['H1', 'L2']
         return values[idx]
 
     def get_fec(self):
         v = self.dep_vars
-        return [(v[0], 'L2_FECollection'),
-                (v[1], 'H1_FECollection'), ]
+        return [(v[0], 'H1_FECollection'),
+                (v[1], 'L2_FECollection'), ]
 
     def postprocess_after_add(self, engine):
         try:
@@ -144,33 +155,36 @@ class RFsheath3D(PhysModule):
         else:
             import warnings
             warnings.warn("Geometry for RFsheath3D must be 3D")
-            return 
+            return
 
     def is_complex(self):
         return self.is_complex_valued
 
     def attribute_set(self, v):
         v = super(RFsheath3D, self).attribute_set(v)
-        v["element"] = 'L2_FECollection, H1_FECollection'
+        v["element"] = 'H1_FECollection, L2_FECollection'
         v["ndim"] = 2
         v["ind_vars"] = 'x, y, z'
         v["dep_vars_suffix"] = ''
-        v["dep_vars_base_txt"] = 'Vsh, Fmag'
+        v["dep_vars_base_txt"] = 'Vsh, Fmg'
         v["is_complex_valued"] = True
+        v["paired_var"] = None
 
         return v
 
     def panel1_param(self):
-        sdim = self.geom_dim
+        from petram.utils import pv_panel_param
 
-        import wx
         panels = super(RFsheath3D, self).panel1_param()
+
         a, b = self.get_var_suffix_var_name_panel()
+        c = pv_panel_param(self, "EM3D1 model")
+
         panels.extend([
             ["independent vars.", self.ind_vars, 0, {}],
             a, b,
             ["derived vars.", ','.join(self.der_vars), 2, {}],
-            ["predefined ns vars.", txt_predefined, 2, {}],])
+            c])
 
         return panels
 
@@ -179,9 +193,12 @@ class RFsheath3D(PhysModule):
         names2 = ', '.join(self.der_vars)
         val = super(RFsheath3D, self).get_panel1_value()
 
+        from petram.utils import pv_get_gui_value
+        gui_value, self.paired_var = pv_get_gui_value(self, self.paired_var)
+
         val.extend([self.ind_vars,
                     self.dep_vars_suffix,
-                    names, names2, txt_predefined,])
+                    names, names2, gui_value, ])
         return val
 
     def import_panel1_value(self, v):
@@ -191,27 +208,29 @@ class RFsheath3D(PhysModule):
         self.ind_vars = str(v[0])
         self.is_complex_valued = False
         self.dep_vars_suffix = str(v[1])
-        self.element = 'L2_FECollection, H1_FECollection'
+        self.element = 'H1_FECollection, L2_FECollection'
         self.dep_vars_base_txt = ', '.join(
             [x.strip() for x in str(v[2]).split(',')])
+
+        from petram.utils import pv_from_gui_value
+        self.paired_var = pv_from_gui_value(self, v[4])
 
         return True
 
     def get_possible_domain(self):
         from petram.phys.rfsheath3d.asymptotic import RFsheath3D_Asymptotic
-        doms = super(RFsheath3D, self).get_possible_domain()
-        doms.extend(RFsheath3D_Asymtptic)
-
+        doms = [RFsheath3D_Asymptotic]
+        doms.extend(super(RFsheath3D, self).get_possible_domain())
         return doms
 
-    '''
     def get_possible_bdry(self):
-        from petram.phys.rfsheath3d.wall import RFsheath3D_Wall
+        from petram.phys.rfsheath3d.essential import RFsheath3D_Essential
+        from petram.phys.rfsheath3d.natural import RFsheath3D_Natural
 
-        bdrs = super(RFsheath3D, self).get_possible_bdry()
-        return [RFsheath3D_Wall]
-    '''
-    
+        bdrs = [RFsheath3D_Essential, RFsheath3D_Natural]
+        bdrs.extend(super(RFsheath3D, self).get_possible_bdry())
+        return bdrs
+
     def get_possible_point(self):
         '''
         To Do. Support point source
