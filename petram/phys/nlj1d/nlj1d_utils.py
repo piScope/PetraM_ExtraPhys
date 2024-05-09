fixed hessian and jacobian to evaluate using normalizaed B vector

petram/phys/nlj1d/nlj1d_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from petram.helper.variables import variable
 delta = 0.001
 
+
 def simple_jacB_1D(f):
     def b_dot(x):
         '''
@@ -9,24 +10,38 @@ def simple_jacB_1D(f):
 
         '''
         ret = np.zeros((3, 3), dtype=np.float64)
-        B = f(x+delta/2.) - f(x-delta/2.)
+
+        B1 = f(x+delta/2.)
+        B1 /= np.sqrt(np.sum(B1**2))
+        B2 = f(x-delta/2.)
+        B2 /= np.sqrt(np.sum(B2**2))
+
+        B = B1 - B2
         B /= delta
         ret[0, 0] = B[0]
         ret[1, 0] = B[1]
         ret[2, 0] = B[2]
-        
+
         return ret.flatten()
     ret = variable.jit.array(shape=(9,))(b_dot)
     return ret
 
+
 def simple_hessB_1D(f):
-    def b_dot2(x):    
+    def b_dot2(x):
         '''
         return B[i,j, k] = dBi/dxj/dxk = \partial_j \partial_k b_i
 
         '''
         ret = np.zeros((3, 3, 3), dtype=np.float64)
-        B = f(x+delta) +f(x-delta) - 2*f(x)
+        B2 = f(x+delta)
+        B2 /= np.sqrt(np.sum(B2**2))
+        B1 = f(x)
+        B1 /= np.sqrt(np.sum(B1**2))
+        B0 = f(x-delta)
+        B0 /= np.sqrt(np.sum(B0**2))
+
+        B = B2 + B0 - 2*B1
         B /= delta
         B /= delta
 
@@ -37,5 +52,3 @@ def simple_hessB_1D(f):
         return ret.flatten()
     ret = variable.jit.array(shape=(27,))(b_dot2)
     return ret
-
-    

petram/phys/nlj2d/nlj2d_utils.py
@@ -10,9 +10,20 @@ def simple_jacB_2D(f):
 
         '''
         ret = np.zeros((3, 3), dtype=np.float64)
-        dBdx = f(x+delta/2., y) - f(x-delta/2., y)
+        B1x = f(x+delta/2., y)
+        B1x /= np.sqrt(np.sum(B1x**2))
+        B2x = f(x-delta/2., y)
+        B2x /= np.sqrt(np.sum(B2x**2))
+
+        dBdx = B1x - B2x
         dBdx /= delta
-        dBdy = f(x, y+delta/2.) - f(x, y-delta/2.)
+
+        B1y = f(x, y+delta/2.)
+        B1y /= np.sqrt(np.sum(B1y**2))
+        B2y = f(x, y-delta/2.)
+        B2y /= np.sqrt(np.sum(B2y**2))
+
+        dBdy = B1y - B2y
         dBdy /= delta
         ret[0, 0] = dBdx[0]
         ret[1, 0] = dBdx[1]
@@ -32,15 +43,42 @@ def simple_hessB_2D(f):
         return B[i,j, k] = dBi/dxj/dxk = \partial_j \partial_k b_i
 
         '''
+        B0 = f(x, y)
+        B0 /= np.sqrt(np.sum(B0**2))
+
+        B1x = f(x+delta, y)
+        B1x /= np.sqrt(np.sum(B1x**2))
+        B2x = f(x-delta, y)
+        B2x /= np.sqrt(np.sum(B2x**2))
+
+        B1y = f(x, y+delta)
+        B1y /= np.sqrt(np.sum(B1y**2))
+        B2y = f(x, y-delta)
+        B2y /= np.sqrt(np.sum(B2y**2))
+
+        B11 = f(x+delta, y+delta)
+        B11 /= np.sqrt(np.sum(B11**2))
+        B12 = f(x+delta, y-delta)
+        B12 /= np.sqrt(np.sum(B12**2))
+        B21 = f(x-delta, y+delta)
+        B21 /= np.sqrt(np.sum(B21**2))
+        B22 = f(x-delta, y-delta)
+        B22 /= np.sqrt(np.sum(B22**2))
+
         ret = np.zeros((3, 3, 3), dtype=np.float64)
-        dBdxx = f(x+delta, y) + f(x-delta, y) - 2*f(x, y)
+        #dBdxx = f(x+delta, y) + f(x-delta, y) - 2*f(x, y)
+        dBdxx = B1x + B2x - 2*B0
         dBdxx /= delta
         dBdxx /= delta
-        dBdyy = f(x, y+delta) + f(x, y-delta) - 2*f(x, y)
+
+        #dBdyy = f(x, y+delta) + f(x, y-delta) - 2*f(x, y)
+        dBdyy = B1y + B2y - 2*B0
         dBdyy /= delta
         dBdyy /= delta
-        dBdxy = (f(x+delta, y+delta) - f(x+delta, y-delta) - 
-                 f(x-delta, y+delta) + f(x-delta, y-delta))
+
+        # dBdxy = (f(x+delta, y+delta) - f(x+delta, y-delta) -
+        #         f(x-delta, y+delta) + f(x-delta, y-delta))
+        dBdxy = B11 - B12 - B21 + B22
         dBdxy /= 2*delta
         dBdxy /= 2*delta
 