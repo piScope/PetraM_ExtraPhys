534d7b4b40e81653cc0571bb3903ba1b8e530409
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-05-19 15:23:41 -0400

    added nlj_vac

---
+++ petram/phys/nlj2d/nlj2d_vac.py
@@ -0,0 +1,44 @@
+from petram.mfem_config import use_parallel
+from petram.phys.common.nlj_mixins import NLJ_Vac
+from mfem.common.mpi_debug import nicePrint
+from petram.phys.vtable import VtableElement, Vtable
+
+import numpy as np
+
+
+import petram.debug as debug
+dprint1, dprint2, dprint3 = debug.init_dprints('NLJ2D_Vac')
+
+if use_parallel:
+    import mfem.par as mfem
+else:
+    import mfem.ser as mfem
+
+
+def domain_constraints():
+    return [NLJ2D_Vac]
+
+
+class NLJ2D_Vac(NLJ_Vac):
+    
+    has_essential = False
+    nlterms = []
+    has_3rd_panel = True
+    
+    def add_bf_contribution(self, engine, a, real=True, kfes=0):
+        
+        if not real:
+            return
+
+        if not real:
+            return
+        
+        dterm = mfem.VectorConstantCoefficient([1, 1, 1.])
+        self.add_integrator(engine, 'mass', dterm, a.AddDomainIntegrator,
+                            mfem.VectorMassIntegrator,)
+        
+        root = self.get_root_phys()
+        dep_var = root.kfes2depvar(kfes)
+        
+        message = "Add mass integrator contribution"
+        dprint1(message, "(real)", dep_var, str(self._sel_index))        
