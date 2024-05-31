39cb387841ec0ac7f87026b9d855ace7b0e71414
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-05-30 23:51:34 -0400

    ELD looks okay

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='PetraM_ExtraPhys',
-    version='1.6.0',
+    version='1.7.0',
 
     description='PetraM RF extra package',
     long_description=long_description,
